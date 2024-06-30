9c2136ee585b8dc48d72db51d0e80fc03d2f29a0
burgerax5 <burgerax3@hotmail.com>
2024-06-30 22:42:31 +1200

    CORS for localhost

--- MTG_Cards/Program.cs
+++ MTG_Cards/Program.cs
@@ -35,6 +35,16 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddHttpClient();
 builder.Services.AddTransient<IScryfallAPI, ScryfallAPI>();
 
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(policy =>
+	{
+		policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+			.AllowAnyHeader()
+			.AllowAnyOrigin(); // For localhost only. Allow all
+	});
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
